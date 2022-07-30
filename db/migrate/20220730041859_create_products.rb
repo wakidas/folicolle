3538c50c4b9d9be5116e3368c1c37382ca899721
Shimpei Wakida <[shimpei.wakida@gmail.com>
2022-07-30 14:56:13 +0900

    feature/24 feat: run rubocop -A
    全体に適用

--- db/migrate/20220730041859_create_products.rb
+++ db/migrate/20220730041859_create_products.rb
@@ -1,8 +1,7 @@
+# frozen_string_literal: true
+
 class CreateProducts < ActiveRecord::Migration[7.0]
   def change
-    create_table :products do |t|
-
-      t.timestamps
-    end
+    create_table :products, &:timestamps
   end
 end
