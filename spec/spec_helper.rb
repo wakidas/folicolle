5b550d28aeaa66af337def3a6ad440fedbec09a1
Shimpei Wakida <[shimpei.wakida@gmail.com>
2022-07-31 22:31:42 +0900

    [feature/39] feat: codecov用のレポートファイル出力用のgem追加

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,6 +18,8 @@
 
 require 'simplecov'
 SimpleCov.start 'rails'
+require 'simplecov-cobertura'
+SimpleCov.formatter = SimpleCov::Formatter::CoberturaFormatter
 
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
