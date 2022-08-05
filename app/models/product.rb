6251fc91037659a590beb9af6497c68639d83427
Shimpei Wakida <[shimpei.wakida@gmail.com>
2022-08-05 13:18:03 +0900

    feature/46 feat: run db:migrate

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: products
+#
+#  id         :bigint           not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Product < ApplicationRecord
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,3 +9,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 end
+
+# == Route Map
+#
