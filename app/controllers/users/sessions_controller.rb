10782a7eb5c58c47775869c2f6e57cd4d7f6b539
Shimpei Wakida <[shimpei.wakida@gmail.com>
2022-08-09 01:23:34 +0900

    feature/64 feat: ログアウト後の遷移先変更

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -6,14 +6,30 @@ class Users::SessionsController < Devise::SessionsController
   # after_action :create_user_login_history!, only: [:create]
 
   # GET /resource/sign_in
+  def new
+    super
+  end
 
   # POST /resource/sign_in
+  def create
+    # binding.pry
+    super
+  end
 
   # DELETE /resource/sign_out
+  def destroy
+    super
+    # binding.pry
+    # redirect_to new_user_session_path, notice: 'ログアウトした'
+  end
 
   private
 
-  def after_sign_in_path_for(resource)
-    stored_location_for(resource) || root_path
+  # def after_sign_in_path_for(resource)
+  #   stored_location_for(resource) || root_path
+  # end
+
+  def after_sign_out_path_for(resource)
+    new_user_session_path
   end
 end
